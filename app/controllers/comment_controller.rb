5f1276aa8f028d4239a2a7047879eac5d9b50930
Chris Grant <chris@qr.io>
2012-02-18 21:15:18 -0500

    began comments controller code

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,2 +1,15 @@
 class CommentController < ApplicationController
+
+  def create
+    @post    = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      flash[:error] = "We were unable to process or save your comment"
+      redirect_to post_path(@post)
+    end
+  end
+
 end
