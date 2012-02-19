25ee165736966f0c8d8735222dc3c7ebc2174ee3
Chris Grant <chris@qr.io>
2012-02-18 21:06:53 -0500

    added User, Post, Comment models and controllers

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  validates :title,  :presence => true
+  validates :url,    :presence => true
+  validates :user_id :presence => true
+
+  belongs_to :user
+  has_many   :comments, :dependent => :delete
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates :name,  :presence => true
+  validates :email, :presence => true
+
+  has_many  :posts
+  has_many  :comments
+end
